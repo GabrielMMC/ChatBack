8dca38c44f35d25b573e00a58a1b7e54247d25a9
GabrielMMC <gabriel.facioni@outlook.com.br>
2023-07-10 18:06:52 -0300

    fix: Cors

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,12 @@ use App\Http\Controllers\UserController;
 Route::group([
     "prefix" => "auth"
 ], function () {
-    Route::post("login", [UserController::class, "login"]);
-    Route::post("register", [UserController::class, "register"]);
+    Route::group([
+        "middleware" => ["auth:api", "cors"]
+    ], function () {
+        Route::post("login", [UserController::class, "login"]);
+        Route::post("register", [UserController::class, "register"]);
+    });
 });
 
 Route::group([
