8a6d8c0915805b270a7aa7ecc563a63ec28bc64b
GabrielMMC <gabriel.facioni@outlook.com.br>
2023-07-10 20:17:15 -0300

    fix: Websockets

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,6 +40,11 @@ return [
                 'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'http',
+                'encrypted' => false,
+                'useTLS' => false,
+            ],
+            'client_options' => [
+                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
             ],
         ],
 
